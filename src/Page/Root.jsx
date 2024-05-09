c2cd957e98338d6a32a412f23c5d587a2e1ce6dd
Md Arif Ahammed Reza <mdarifahammedreza@gmail.com>
2024-05-09 23:28:58 +0600

    console.log

--- src/Page/Root.jsx
+++ src/Page/Root.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Header from "../Component/Header/Header";
 import Marquee from "react-fast-marquee";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const Root = () => {
   console.log("Root");
   return (
     <div>
       <Analytics />
+      <SpeedInsights />
       <div className="bg-transparent">
         <Marquee>
           <p>
